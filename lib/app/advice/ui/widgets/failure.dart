60c5a5925cd3e348973dc9a102dd18d3eb91fabf
JosephKorel <jcjosecarlos03@gmail.com>
2023-09-13 11:37:34 -0300

    feat: add animations to connection failure view

--- lib/app/advice/ui/widgets/failure.dart
+++ lib/app/advice/ui/widgets/failure.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_symbols_icons/symbols.dart';
 import 'package:sophis/app/advice/presenter/bloc/advice_bloc.dart';
 import 'package:sophis/app/core/error.dart';
 
@@ -34,11 +36,43 @@ class ConnectionFailureView extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: Column(
         children: [
-          const Text('Seems like your connection is unstable'),
-          FilledButton(
-            onPressed: () {},
-            child: const Text('Try again'),
+          const Icon(
+            Symbols.signal_disconnected_rounded,
+            size: 64,
+          )
+              .animate(
+                onComplete: (controller) => controller.repeat(),
+              )
+              .shake(delay: 1.seconds, duration: 1.5.seconds),
+          const SizedBox(
+            height: 16,
           ),
+          Text(
+            'Seems like your connection is unstable',
+            style: Theme.of(context).textTheme.titleLarge,
+            textAlign: TextAlign.center,
+          ).animate().fadeIn(),
+          const SizedBox(
+            height: 16,
+          ),
+          FilledButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.replay_outlined),
+            label: const Text('Try again'),
+          ).animate().fadeIn().slideY(
+                begin: 8,
+                end: 0,
+                curve: Curves.easeOutCirc,
+              ),
+          TextButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.home),
+            label: const Text('Home'),
+          ).animate().fadeIn(delay: .2.seconds).slideY(
+                begin: 6,
+                end: 0,
+                curve: Curves.easeOutCirc,
+              ),
         ],
       ),
     );
